35222edd72bbf0943135b62b07f4081a439370d2
WimEvers <134300435+WimEvers@users.noreply.github.com>
2025-05-30 11:42:52 +0200

    zoom to regio implemented

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 function init(){
     //alert("init called");
  }
-/*
-function zoomregio() {
-  //kijk welke popup open is
+
+function zoomregioa() {
+  alert("clicked in window")
+  for (var i =0;  i< vakregio.length; ++i) {
+    if (markerlist[i].isPopupOpen()) {
+      var regiodetail = vakregio[i][3]
+      alert("regio selected is " + regiodetail)
+      //hier komt de loop over een specifiek regio, in eerste instantie alleen voor Canarische eilanden
+      if (regiodetail = "Canarische eilanden") {
+        alert("in if statement")
+        //mymap.setView([28.683,-16.337], 8);
+        mymap.setView([vakregio[i][0],vakregio[i][1]], vakregio[i][2]);  // deze regio
+      }
+    }
+    else {
+      console.log("popup niet open "+vakregio[i][3])
+    }
+  }
+}
+  /*//kijk welke popup open is
   // Welke regio is popup open?
   if(marker.isPopupOpen()) {
   // do something
@@ -14,36 +31,37 @@ function zoomregio() {
 } */
 
 //alert("js initialized");
-var map = L.map('map').setView([37.09,-0.53], 3);  // hele wereld = geo:37.09,-0.53?z=3
+var mymap = L.map('map').setView([10.0,15.0], 3);  // hele wereld = geo:37.09,-0.53?z=3
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+  }).addTo(mymap);
 
 /*Data vakanties 
 formaat: 
 latitude, langitude, zoom, regio*/
 const vakregio = [
-[28.683,  -16.337,  8, "Canarische eilanden"],
+[28.683,  -16.337,  9, "Canarische eilanden"],
 [56.502,  -3.9220,  7, "Schotland"],
 [39.371,   2.7430,  9, "Mallorca"]
 ];
 
 var regionaam;
+var regiofound = "none";
+
 const markerlist = [];
 console.log(markerlist.length)
 for (var i = 0; i < vakregio.length; ++i) {
-/*for (var j = 0; j < vakregio[i].length; ++j) {*/
     regionaam="<p>"+vakregio[i][3]+"</p>";
     console.log(regionaam);
-    markerA = L.marker([vakregio[i][0], vakregio[i][1]]).addTo(map).bindPopup(regionaam).openPopup();
+    markerA = L.marker([vakregio[i][0], vakregio[i][1]]).addTo(mymap).bindPopup(regionaam).openPopup();
     console.log(markerA)
     markerlist.push(markerA);
-  /*}*/
 }
+
 console.log(markerlist.length)
 
-map.closePopup()
+mymap.closePopup()
 
 /*Data locaties 
 formaat: 
@@ -53,21 +71,6 @@ const locaties = [
 [28.28526,-16.43538 , "Guimar",    "Canarische eilanden", "Spanje", 2025, "April"],
 [28.14428,-17.21317 , "Hermigua",  "Canarische eilanden", "Spanje", 2025, "April"] ];
 
-
-
-//hier komt de loop over een specifiek regio
-
-
-/*var markerLS = 
-L.marker([28.35221,-16.835699]).addTo(map).bindPopup("<p>Los Silos</p>").openPopup();
-var markerG = L.marker([28.28526,-16.43538]).addTo(map).bindPopup("<p>Guimar</p>").openPopup();
-var markerH = L.marker([28.14428,-17.21317]).addTo(map).bindPopup("<p>Hermigua</p><div><img src='image/hermigua.png'/></div>").openPopup();
-// blijkbaar kan ik ook een functie hier ingeven, deze functie moet dan wel een html element terug geven wat in de overlay gebruikt kan worden
-
-    
-//markerLS.bindPopup("Los Silos").openPopup();
-//markerG.bindPopup("Guimar").openPopup();
-//markerH.bindPopup("Hermigua").openPopup();
 
 
 // screen size
@@ -81,11 +84,13 @@ function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.sourceTarget.toString())
-        .openOn(map);
+        .openOn(mymap);
 }
 
-map.on('click', onMapClick);  
-*/
+mymap.on('click', onMapClick);  
+
 
 window.addEventListener('load', init);
-window.addEventListenet('click', zoomregio)
+document.getElementById("map").addEventListener("click", zoomregioa);
+
+
